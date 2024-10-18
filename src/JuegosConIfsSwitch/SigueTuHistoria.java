a6da2c390a6f7d6bdfeb454d19243ddd4e4df040
Jose Antonio 'LR' Paz <jopax@jopax.dev>
2024-10-18 19:37:30 +0200

    WIP

---
+++ src/JuegosConIfsSwitch/SigueTuHistoria.java
@@ -0,0 +1,38 @@
+package JuegosConIfsSwitch;
+
+public class SigueTuHistoria {
+
+    /*
+        Reglas basicas: Empieza la aventura y el usuario va eligiendo en funcion de una serie de opciones para ver a que
+                        pagina sera redirigido.
+
+        Limitaciones:
+            - Solo se permite el uso de tipos que hemos visto (int, String, double....)
+            - Solo se pueden usar ifs y switch
+
+        Ayuda:
+            - Seguiremos la aventura hasta que el usuario llega al final.
+            - Estamos limitados por tipos de datos vistos hasta ahora y por estructuras vistas hasta ahora.
+            - Para generar la historia y las opciones/enlaces a cada pagina, usaremos chatgpt ya que lo importante es
+                la lógica del programa, no el texto en si.
+     */
+
+    public static void main(String[] args) {
+
+        /*
+            Definimos 3 variables
+            - En una guardaremos la pagina a la que vamos.
+            - En la otra, el numero de decisiones que tomamos hasta llegar al final.
+         */
+        int pagina, decisiones = 0;
+        boolean finAventura = false;
+
+        /*
+            Mientras que no sea el fin de la aventura ejecutamos en bucle las opciones
+            Cuando lleguemos a un final, cambiaremos la variable de valor para que nos saque del bucle.
+         */
+        while (!finAventura){
+
+        }
+    }
+}
